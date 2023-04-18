097351f2b949160d22262ecd8a6b8926bc78a534
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-04-18 18:04:35 +0300

    Basic structure with minimum style

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -2,49 +2,49 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    missionsData:
+  missionsData:
     [
       {
-      mission_name: 'Thaicom',
-      mission_id: '9D1B7E',
-      manufacturers: ['Orbital ATK'],
-      payload_ids: ['Thaicom 6', 'Thaicom 8'],
-      wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
-      website: 'http://www.thaicom.net/en/satellites/overview',
-      twitter: 'https://twitter.com/thaicomplc',
-      description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-    },
-    {
-      mission_name: 'Thaicom',
-      mission_id: '9D1B7E0',
-      manufacturers: ['Orbital ATK'],
-      payload_ids: ['Thaicom 6', 'Thaicom 8'],
-      wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
-      website: 'http://www.thaicom.net/en/satellites/overview',
-      twitter: 'https://twitter.com/thaicomplc',
-      description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-    },
-    {
-      mission_name: 'Thaicom',
-      mission_id: '9D1B7E0',
-      manufacturers: ['Orbital ATK'],
-      payload_ids: ['Thaicom 6', 'Thaicom 8'],
-      wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
-      website: 'http://www.thaicom.net/en/satellites/overview',
-      twitter: 'https://twitter.com/thaicomplc',
-      description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-    },
-    {
-      mission_name: 'Thaicom',
-      mission_id: '9D1B7E0',
-      manufacturers: ['Orbital ATK'],
-      payload_ids: ['Thaicom 6', 'Thaicom 8'],
-      wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
-      website: 'http://www.thaicom.net/en/satellites/overview',
-      twitter: 'https://twitter.com/thaicomplc',
-      description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-    }
-  ],
+        mission_name: 'Thaicom',
+        mission_id: '9D1B7E',
+        manufacturers: ['Orbital ATK'],
+        payload_ids: ['Thaicom 6', 'Thaicom 8'],
+        wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
+        website: 'http://www.thaicom.net/en/satellites/overview',
+        twitter: 'https://twitter.com/thaicomplc',
+        description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+      },
+      {
+        mission_name: 'Thaicom',
+        mission_id: '9D1B7E0',
+        manufacturers: ['Orbital ATK'],
+        payload_ids: ['Thaicom 6', 'Thaicom 8'],
+        wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
+        website: 'http://www.thaicom.net/en/satellites/overview',
+        twitter: 'https://twitter.com/thaicomplc',
+        description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+      },
+      {
+        mission_name: 'Thaicom',
+        mission_id: '9D1B7E0',
+        manufacturers: ['Orbital ATK'],
+        payload_ids: ['Thaicom 6', 'Thaicom 8'],
+        wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
+        website: 'http://www.thaicom.net/en/satellites/overview',
+        twitter: 'https://twitter.com/thaicomplc',
+        description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+      },
+      {
+        mission_name: 'Thaicom',
+        mission_id: '9D1B7E0',
+        manufacturers: ['Orbital ATK'],
+        payload_ids: ['Thaicom 6', 'Thaicom 8'],
+        wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
+        website: 'http://www.thaicom.net/en/satellites/overview',
+        twitter: 'https://twitter.com/thaicomplc',
+        description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
+      },
+    ],
   isSubscribed: false,
 };
 
@@ -54,7 +54,7 @@ export const missionsSlice = createSlice({
   reducers: {
     subscribe(state, action) {
       state.isSubscribed != isSubscribed;
-    }
+    },
   },
 });
 
