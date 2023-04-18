87aac6664d1d6ff892f5889bf0845ea6c5feed9d
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-04-18 17:03:02 +0300

    Basic structure with minimum styling

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,23 @@
+/* eslint-disable */
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { MissionItemHead } from './missionItemsHead';
+import { MissionItems } from './MissionItemsData';
 
 function Missions() {
+  const data = useSelector((store) => store.missions.missionsData);
+  
   return (
-    <div>Missions</div>
+    <div >
+      <MissionItemHead />
+      <ul>
+            {data.map((item) => 
+                <MissionItems key={item.mission_id} item={item} />
+            )}
+        </ul>
+      
+    </div>
+    
   );
 }
 
---
+++ src/components/missionItemsHead.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from "react";
+
+export const MissionItemHead = () => {
+    return (
+        <div className="mission-headers">
+            <div><p>Mission</p></div>
+            <div className="mission-disc"><p >Description</p></div>
+            <div><p>Status</p></div>
+            <div><p></p></div>
+        </div>
+    )
+}
