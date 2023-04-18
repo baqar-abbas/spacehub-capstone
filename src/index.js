b8e0f70a5eb64b8c5d07585a637275bd85c93408
baqar-abbas <baqar007@hotmail.com>
2023-04-18 14:13:11 +0500

    Styleint linter error fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import './index.css';
+// import './index.css';
 import App from './App';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
