304cf6ae602f99dd3ddd343bc311301a0842649a
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-04-19 10:58:22 +0300

    fetch missions data from api

--- src/redux/missions/missionsSlice.js
+++ src/redux/missions/missionsSlice.js
@@ -1,61 +1,41 @@
 /* eslint-disable */
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const url = 'https://api.spacexdata.com/v3/missions'
+
+export const fetchMission = createAsyncThunk('post/fetchPost', async () => {
+  return fetch(url)
+  .then(res => res.json());
+})
 
 const initialState = {
-  missionsData:
-    [
-      {
-        mission_name: 'Thaicom',
-        mission_id: '9D1B7E',
-        manufacturers: ['Orbital ATK'],
-        payload_ids: ['Thaicom 6', 'Thaicom 8'],
-        wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
-        website: 'http://www.thaicom.net/en/satellites/overview',
-        twitter: 'https://twitter.com/thaicomplc',
-        description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-      },
-      {
-        mission_name: 'Thaicom',
-        mission_id: '9D1B7E0',
-        manufacturers: ['Orbital ATK'],
-        payload_ids: ['Thaicom 6', 'Thaicom 8'],
-        wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
-        website: 'http://www.thaicom.net/en/satellites/overview',
-        twitter: 'https://twitter.com/thaicomplc',
-        description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-      },
-      {
-        mission_name: 'Thaicom',
-        mission_id: '9D1B7E0',
-        manufacturers: ['Orbital ATK'],
-        payload_ids: ['Thaicom 6', 'Thaicom 8'],
-        wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
-        website: 'http://www.thaicom.net/en/satellites/overview',
-        twitter: 'https://twitter.com/thaicomplc',
-        description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-      },
-      {
-        mission_name: 'Thaicom',
-        mission_id: '9D1B7E0',
-        manufacturers: ['Orbital ATK'],
-        payload_ids: ['Thaicom 6', 'Thaicom 8'],
-        wikipedia: 'https://en.wikipedia.org/wiki/Thaicom',
-        website: 'http://www.thaicom.net/en/satellites/overview',
-        twitter: 'https://twitter.com/thaicomplc',
-        description: 'Thaicom is the name of a series of communications satellites operated from Thailand, and also the name of Thaicom Public Company Limited, which is the company that owns and operates the Thaicom satellite fleet and other telecommunication businesses in Thailand and throughout the Asia-Pacific region. The satellite projects were named Thaicom by the King of Thailand, His Majesty the King Bhumibol Adulyadej, as a symbol of the linkage between Thailand and modern communications technology.',
-      },
-    ],
+  missionsData: [],
   isSubscribed: false,
+  isLoading: false,
+  error: ''
 };
 
 export const missionsSlice = createSlice({
   name: 'missions',
   initialState,
   reducers: {
-    subscribe(state, action) {
-      state.isSubscribed != isSubscribed;
+    subscribe(state) {
+      state.isSubscribed = true;
     },
   },
+  extraReducers: (builder) =>{
+    builder.addCase(fetchMission.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(fetchMission.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.missionsData = action.payload;
+    });
+    builder.addCase(fetchMission.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.error.message;
+    })
+  }
 });
 
 export default missionsSlice.reducer;
