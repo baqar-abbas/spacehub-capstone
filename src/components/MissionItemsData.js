38691470ecd927b96cb16c81ef479136fb47165b
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-04-19 17:49:52 +0300

    final styling

--- src/components/MissionItemsData.js
+++ src/components/MissionItemsData.js
@@ -1,37 +1,40 @@
-/* eslint-disable */
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { subscribe } from '../redux/missions/missionsSlice';
 
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { subscribe } from '../redux/missions/missionsSlice'
+const MissionItems = (props) => {
+  const [subscribed, setSubscribed] = useState(!props.isSubscribed);
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(subscribe(props.item.mission_id));
+    setSubscribed((prev) => prev = !prev);
+  };
 
-export const MissionItems = (props) => {
-    const [subscribed, setSubscribed] = useState(!props.isSubscribed)
-    const dispatch = useDispatch();
-    const handleClick = () => {
-        dispatch(subscribe(props.item.mission_id));
-        setSubscribed((prev) => prev = !prev)
-    }
+  const style = {
+    backgroundColor: subscribed ? 'rgb(65, 65, 236)' : 'rgb(145, 139, 139)',
+  };
+  const btnStyle = {
+    borderColor: subscribed ? 'red' : 'black',
+    color: subscribed ? 'red' : 'black',
+  };
 
-    const style = {
-        backgroundColor: subscribed ? 'rgb(65, 65, 236)' : 'rgb(145, 139, 139)',
-    };
-    const btnStyle = {
-        borderColor: subscribed ? 'red' : 'black',
-        color: subscribed ? 'red' : 'black',
-    }
-
-    return (
-        <div className="mission-headers">
-            <div><p>{props.item.mission_name}</p></div>
-            <div className="mission-disc"><p >{props.item.description}</p></div>
-            <div><p className="active-toggle" style={style}>{subscribed ? 'Active Member' : 'Not a member'}</p></div>
-            <div>
-                <p>
-                    <button className="subscribe-btn" onClick={handleClick} style={btnStyle}>
-                        {subscribed ? 'Leave Mission' : 'Join Mission'}
-                    </button>
-                </p>
-            </div>
+  return (
+    <div className="alternate-clr">
+      <div className="mission-headers">
+        <div><p>{props.item.mission_name}</p></div>
+        <div className="mission-disc"><p>{props.item.description}</p></div>
+        <div><p className="active-toggle" style={style}>{subscribed ? 'Active Member' : 'Not a member'}</p></div>
+        <div>
+          <p>
+            <button type="button" className="subscribe-btn" onClick={handleClick} style={btnStyle}>
+              {subscribed ? 'Leave Mission' : 'Join Mission'}
+            </button>
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+
+  );
+};
+
+export default MissionItems;
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,39 +1,36 @@
-/* eslint-disable */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { MissionItemHead } from './missionItemsHead';
-import { MissionItems } from './MissionItemsData';
+import MissionItems from './MissionItemsData';
 import { fetchMission } from '../redux/missions/missionsSlice';
 
 function Missions() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchMission())
+    dispatch(fetchMission());
   }, [dispatch]);
 
   const data = useSelector((store) => store.missions.missionsData);
-  const isLoading = useSelector((store) => store.missions.isLoading)
-  const error = useSelector((store) => store.missions.error)
-  const isSubscribed = useSelector((store) => store.missions.isSubscribed)
-  
+  const isLoading = useSelector((store) => store.missions.isLoading);
+  const error = useSelector((store) => store.missions.error);
+  const isSubscribed = useSelector((store) => store.missions.isSubscribed);
+
   if (isLoading) {
-    return 'loading...'
-  };
+    return 'loading...';
+  }
   if (error) {
-    return error
-  };
+    return error;
+  }
 
   return (
-    <div className='missions-container'>
+    <div className="missions-container">
       <MissionItemHead />
       <ul>
-        {data.map((item) => 
-            <MissionItems key={item.mission_id} item={item} isSubscribed={isSubscribed}/>
-        )}
+        {data.map((item) => <MissionItems key={item.mission_id} item={item} isSubscribed={isSubscribed} />)}
       </ul>
     </div>
-    
+
   );
 }
 
