b69106b226ddcd8d381ddc9ffb230adf21f6d729
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-04-19 15:36:29 +0300

    Missions added in Profile Page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,23 @@
+/* eslint-disable */
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-function Profile() {
+const Profile = () => {
+  const missions = useSelector((store) => store.missions.missionsData);
+  const filteredMissions = missions.filter((mission) => mission.isSubscribed === true);
+
   return (
-    <div>Profile</div>
+    <main>
+      <div className="subscribed-missions">
+        <h3>My Missions</h3>
+        <ul>
+          { filteredMissions.map((mission) => {
+            return <li key={mission.mission_id}>{mission.mission_name}</li>;
+          }) }
+        </ul>
+      </div>
+    </main>
   );
 }
 
-export default Profile;
+export default Profile
